8ab73fb55af61f26e9b41bc36e9a407caf6eb785
parth-maheta <parthmaheta.1012@gmail.com>
2025-05-27 15:18:48 +0530

    Auth implementation

--- pricepulse-frontend/src/api/axios.js
+++ pricepulse-frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://localhost:5000/api",
+  baseURL:
+    "https://pricepulse-qaeh.onrender.com/api" || "https://localhost:5000/api",
 });
 
 instance.interceptors.request.use((config) => {
