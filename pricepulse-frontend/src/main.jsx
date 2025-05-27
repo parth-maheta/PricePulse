2a856045d82e162ea0b32d3037590cf69750f9e2
parth-maheta <parthmaheta.1012@gmail.com>
2025-05-27 19:09:52 +0530

    Auth implementation

--- pricepulse-frontend/src/main.jsx
+++ pricepulse-frontend/src/main.jsx
@@ -4,12 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const clerkKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={clerkKey}>
-      <App />
-    </ClerkProvider>
+    <App />
   </StrictMode>
 );
